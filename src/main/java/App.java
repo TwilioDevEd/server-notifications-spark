442c82030c70623b8c90a15259c7536010ff5321
Agustin Camino <agustin.camino@gmail.com>
2015-09-22 10:57:56 -0500

    Create a custom alert message

---
+++ src/main/java/App.java
@@ -0,0 +1,20 @@
+import static spark.Spark.exception;
+import static spark.Spark.get;
+
+public class App {
+    public static void main(String[] args) {
+        get("/", (request, response) -> {
+            throw new Exception("Impossible happened");
+        });
+
+        // http://sparkjava.com/documentation.html#exception-mapping
+        exception(Exception.class, (e, request, response) -> {
+            String message = String.format("It appears the server is having Exception: %s " +
+                    "Go to: http://newrelic.com for more details. " +
+                    "Image URL: http://goo.gl/ObTXdX",
+                    e.getMessage());
+
+            response.body("Something unexpected happened. Keep calm, administrators were notified.");
+        });
+    }
+}
